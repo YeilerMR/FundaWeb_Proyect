cd031c1f3ad9c1d824610c56c101f3c28baab97b
Aaron Matarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2025-10-22 00:07:28 -0600

    feat: Add admin routes and slider resource controller
    Introduced an 'admin' route group with a dashboard view and resource routes for the SliderController. This sets up basic admin routing structure for future development.

--- Komercia/routes/web.php
+++ Komercia/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SliderController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', fn() => view('admin.dashboard.index'))->name('dashboard');
+    Route::resource('slider', SliderController::class)->names('slider');
 });
